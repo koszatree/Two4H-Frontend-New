a17fc96bbf9f20de051e4bead44a95f97b4d49b0
koszatree <koszatree@gmail.com>
2024-04-22 23:01:36 +0200

    Init

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { User } from '../user/user';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  user: User = new User();
+
+  constructor(private http: HttpClient) {}
+
+  Login() {
+      let bodyData = {
+        email: this.user.email,
+        password: this.user.password
+      };
+
+      this.http.post("http://localhost:8080/api/login", bodyData).subscribe((resultData: any) => {
+        if(resultData.status == false) {
+          alert("Incorrect email or password!");
+        }
+        else {
+          alert("Logged succesfully!");
+          //this.router.navigateByUrl("/home");
+        }
+      });
+  }
+}
