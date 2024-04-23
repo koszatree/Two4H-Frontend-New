d312f3e09fd170c09832d2c0732c8a20bb21b9dc
Mixxu <teemo1709@interia.pl>
2024-04-23 19:00:25 +0200

    added order and map path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { AboutComponent } from './about/about.component';
+import { MapComponent } from './map/map.component';
+import { OrderComponent } from './order/order.component';
+
 
 const routes: Routes = [{
   path: '',
@@ -24,6 +27,16 @@ const routes: Routes = [{
 {
   path: 'about',
   component: AboutComponent,
+},
+
+{
+  path: 'map',
+  component: MapComponent,
+},
+
+{
+  path: 'order',
+  component: OrderComponent,
 }
 
 ];
