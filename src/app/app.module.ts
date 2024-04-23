e8c25d37ba0b03d182adef0496e8cdc46143f6d0
koszatree <koszatree@gmail.com>
2024-04-23 16:33:15 +0200

    Structure changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,21 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { RegisterComponent } from './register/register.component';
+import { AboutComponent } from './about/about.component';
+import { OrderComponent } from './order/order.component';
+import { HomeComponent } from './home/home.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    AboutComponent,
+    OrderComponent,
+    HomeComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
